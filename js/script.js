a231bc2f734b67f10d780fec15bdd5df8cf4ad04
artisyn <artioms1@gmail.com>
2022-01-23 00:04:12 +0200

    figured out the layout and some js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+const crossMenu = document.querySelector('.sidebar--menu');
+const colorOptions = document.querySelectorAll('.sidebar--color');
+crossMenu.addEventListener('click', function (e) {
+  if (e.target.checked === true) {
+    colorOptions.forEach((el) => {
+      el.classList.remove('displayNone');
+    });
+  }
+  if (e.target.checked === false) {
+    colorOptions.forEach((el) => {
+      el.classList.add('displayNone');
+    });
+  }
+});
+
+colorOptions.forEach((el) => {
+  el.addEventListener('click', function (e) {
+    const { color } = e.target.dataset; // getting color var
+  });
+});
