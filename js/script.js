a80841cedd96fe8323b7355549b6e4ac576b8831
artisyn <artioms1@gmail.com>
2022-01-26 22:47:11 +0200

    refactored alot

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ const mainDisplay = document.querySelector('.mainDisplay');
 crossMenu.addEventListener('click', function (e) {
   if (e.target.checked === true) {
     colorOptions.forEach((el) => {
-      el.classList.remove('displayNone');
+      el.classList.remove('NoOpacity');
     });
   }
   if (e.target.checked === false) {
     colorOptions.forEach((el) => {
-      el.classList.add('displayNone');
+      el.classList.add('NoOpacity');
     });
   }
 });
@@ -25,7 +25,7 @@ colorOptions.forEach((el) => {
 
 const createTemplate = function (color) {
   const div = document.createElement('div');
-  div.classList.add('note', 'displayNone');
+  div.classList.add('note', 'NoOpacity');
   let time = new Intl.DateTimeFormat('lt', {
     dateStyle: 'short',
     timeStyle: 'short', //short, long
@@ -41,7 +41,7 @@ const createTemplate = function (color) {
 						rows="1"
 						cols="20"
 						placeholder="Title..."
-                        disabled="true"
+                        
 					></textarea>
 					<textarea
 						style="resize: none"
@@ -49,35 +49,63 @@ const createTemplate = function (color) {
 						cols="30"
 						rows="10"
                         placeholder="Your notes..."
-                        disabled="true"
+                        
 					></textarea>
 					<span class="note--date">${time}</span>
-					<span class="note--edit"
+					<span class="note--save"><i class="far fa-save"></i></span>
+					<span class="note--edit displayNone"
 						><i class="fas fa-pencil-alt"></i
 					></span>
                     `;
   div.innerHTML = markup;
   mainDisplay.prepend(div);
   setTimeout(() => {
-    mainDisplay.firstChild.classList.remove('displayNone');
+    mainDisplay.firstChild.classList.remove('NoOpacity');
   }, 200);
 };
 
-mainDisplay.addEventListener('mouseover', (e) => {
-  if (e.target.classList.contains('note')) {
-    let text = [e.target.children[1], e.target.children[2]];
-    // showing delete icon
-    e.target.children[0].classList.remove('displayNone');
-    // adding click response
-    e.target.children[0].addEventListener('click', (e) => e.path[2].remove());
-    // adding click for editing
-    e.target.children[4].addEventListener('click', (e) => {
-      text.forEach((el) => (el.disabled = false));
-    });
-    //hiding delete icon + prevent editing
-    e.target.addEventListener('mouseleave', (e) => {
-      e.target.children[0].classList.add('displayNone');
-      text.forEach((el) => (el.disabled = true));
-    });
-  }
+// need to remaster event listener
+mainDisplay.addEventListener('click', (e) => {
+  console.log(e.target);
 });
+
+// mainDisplay.addEventListener('mouseover', (e) => {
+//   if (e.target.classList.contains('note')) {
+//     console.log(e);
+//     // selecting text input areas
+//     let text = [e.target.children[1], e.target.children[2]];
+//     console.log(text);
+//     // showing delete icon
+//     e.target.children[0].classList.remove('displayNone');
+//     // adding click response
+//     e.target.children[0].addEventListener('click', (e) => e.path[2].remove());
+
+//     // adding save click functionality
+//     e.target.children[4].addEventListener('click', (e) => {
+//       //disabling edit
+//       text.forEach((el) => {
+//         console.log(el.disabled);
+//         el.disabled = true;
+//         console.log(el.disabled);
+//       });
+//       //hiding save button
+//       console.log(e.target);
+//     });
+
+//     // adding click for editing  4-save, 5 - edit
+
+//     // e.target.children[4].addEventListener('click', (e) => {
+//     //   text.forEach((el) => (el.disabled = false));
+//     // });
+//     //hiding delete icon + prevent editing
+//     e.target.addEventListener('mouseleave', (e) => {
+//       e.target.children[0].classList.add('NoOpacity');
+//       // text.forEach((el) => (el.disabled = true));  // preventing editing
+//     });
+//   }
+// });
+
+//TODO
+// add an alert or smt on delete buttton
+// refigure all event listeners
+// add a local storage save option
