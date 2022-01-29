82455ea7f11130bf5e958fbc621f7828b61c50ce
artisyn <artioms1@gmail.com>
2022-01-29 02:20:33 +0200

    only user guide left

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@
 const crossMenu = document.querySelector('.sidebar--menu');
 const colorOptions = document.querySelectorAll('.sidebar--color');
 const mainDisplay = document.querySelector('.mainDisplay');
+
+//generating random ID;
+const createId = () => {
+  const letters = ['a', 'b', 'c', 'd', 'x', 'y', 'z'];
+  const id =
+    letters[Math.floor(Math.random() * letters.length)] +
+    Math.floor(Math.random() * 999999);
+  return id;
+};
+
 // for switching between save and edit;
 function flipIcons(icon1, icon2) {
   [icon1, icon2].forEach((icon) => {
@@ -10,6 +20,78 @@ function flipIcons(icon1, icon2) {
     } else {
       icon.classList.add('displayNone');
     }
+  });
+}
+
+// local storage
+
+function getValues(note) {
+  console.log(note);
+  // date, title value, text value, bg color;
+  const noteTitle = note.children[1].value;
+  const noteText = note.children[2].value;
+  const noteDate = note.children[3].innerHTML;
+  const notebackground = note.style.backgroundColor;
+  const noteId = note.id;
+
+  return {
+    title: noteTitle,
+    text: noteText,
+    date: noteDate,
+    color: notebackground,
+    id: noteId,
+  };
+}
+const addNotesFromLs = function (id, title, text, date, color) {
+  const div = document.createElement('div');
+  div.classList.add('note');
+  div.id = id;
+  div.style.backgroundColor = color;
+  const markup = `
+					<span class="note--delete displayNone"
+						><i class="far fa-trash-alt"></i
+					></span>
+					<textarea
+						class="note--title"
+						style="resize: none"
+						// rows="2"
+						// cols="20"
+            maxlength="22"
+						placeholder="Title..."
+                        
+					></textarea>
+					<textarea
+						style="resize: none"
+						class="note--text"
+						cols="30"
+						rows="10"
+            placeholder="Your notes..." 
+            
+					></textarea>
+					<span class="note--date">${date}</span>
+					<span class="note--save"><i class="far fa-save"></i></span>
+					<span class="note--edit displayNone"
+						><i class="fas fa-pencil-alt"></i
+					></span>
+                    `;
+  div.innerHTML = markup;
+
+  div.children[1].innerText = title;
+  div.children[2].innerText = text;
+
+  mainDisplay.prepend(div);
+};
+function loadNotesFromLs() {
+  const keys = Object.keys({ ...localStorage });
+  keys.forEach((key) => {
+    const note = JSON.parse(localStorage.getItem(key));
+    const [title, text, date, color] = [
+      note.title,
+      note.text,
+      note.date,
+      note.color,
+    ];
+    addNotesFromLs(key, title, text, date, color);
   });
 }
 
@@ -51,6 +133,7 @@ colorOptions.forEach((el) => {
 const createTemplate = function (color) {
   const div = document.createElement('div');
   div.classList.add('note', 'noOpacity');
+  div.id = createId();
   let time = new Intl.DateTimeFormat('lt', {
     dateStyle: 'short',
     timeStyle: 'short', //short, long
@@ -74,7 +157,7 @@ const createTemplate = function (color) {
 						class="note--text"
 						cols="30"
 						rows="10"
-                        placeholder="Your notes..."
+            placeholder="Your notes..."
                         
 					></textarea>
 					<span class="note--date">${time}</span>
@@ -92,12 +175,16 @@ const createTemplate = function (color) {
 
 // need to remaster event listener
 mainDisplay.addEventListener('click', (e) => {
-  // console.log(e.target);
   // Selecting Delete icon
   if (e.target.classList.contains('fa-trash-alt')) {
+    const note = e.target.closest('.note');
     if (window.confirm('Delete Note?')) {
+      // Deleting locacStorage if there
+      localStorage.getItem(note.id) !== null
+        ? localStorage.removeItem(note.id)
+        : '';
       // Deleting note element
-      e.target.closest('.note').remove();
+      note.remove();
     }
   }
   // Selecting save icon
@@ -105,6 +192,9 @@ mainDisplay.addEventListener('click', (e) => {
     const note = e.target.closest('.note');
     flipTextAreas(e.target);
     flipIcons(note.children[4], note.children[5]);
+    // Add to localStorage
+    const lsNote = JSON.stringify(getValues(note));
+    localStorage.setItem(note.id, lsNote);
   }
   // Selecting edit button
   if (e.target.classList.contains('fa-pencil-alt')) {
@@ -122,44 +212,9 @@ mainDisplay.addEventListener('dblclick', (e) => {
   console.log(e);
 });
 
-// mainDisplay.addEventListener('mouseover', (e) => {
-//   if (e.target.classList.contains('note')) {
-//     console.log(e);
-//     // selecting text input areas
-//     let text = [e.target.children[1], e.target.children[2]];
-//     console.log(text);
-//     // showing delete icon
-//     e.target.children[0].classList.remove('displayNone');
-//     // adding click response
-//     e.target.children[0].addEventListener('click', (e) => e.path[2].remove());
+// Pulling notes from local Storage
 
-//     // adding save click functionality
-//     e.target.children[4].addEventListener('click', (e) => {
-//       //disabling edit
-//       text.forEach((el) => {
-//         console.log(el.disabled);
-//         el.disabled = true;
-//         console.log(el.disabled);
-//       });
-//       //hiding save button
-//       console.log(e.target);
-//     });
-
-//     // adding click for editing  4-save, 5 - edit
-
-//     // e.target.children[4].addEventListener('click', (e) => {
-//     //   text.forEach((el) => (el.disabled = false));
-//     // });
-//     //hiding delete icon + prevent editing
-//     e.target.addEventListener('mouseleave', (e) => {
-//       e.target.children[0].classList.add('noOpacity');
-//       // text.forEach((el) => (el.disabled = true));  // preventing editing
-//     });
-//   }
-// });
+loadNotesFromLs();
 
 //TODO
-// add an alert or smt on delete buttton done
-// refigure all event listeners done
-// expand on double click done
-// add a local storage save option
+// add some guidelines for user
